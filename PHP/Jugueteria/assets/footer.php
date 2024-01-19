25cecbc810aae89fb508ef06244bc4523a3d6b5f
Miguel Esteban <miguelestebn@gmail.com>
2024-01-19 18:44:34 +0100

    aaa

---
+++ PHP/Jugueteria/assets/footer.php
@@ -0,0 +1,10 @@
+</main>
+
+<footer>
+<p>Estas en el apartado > <?php echo $titulin;?></p>    
+<?php include 'assets/menu.php';?>
+    <p>&copy; Copyright <?php echo date('Y')?></p>
+</footer>
+
+</body>
+</html>
---
+++ PHP/Jugueteria/assets/menu.php
@@ -0,0 +1,16 @@
+<nav>
+    <ul>
+         <li>
+            <a href="index.php">Home</a>
+         </li>
+         <li>
+            <a href="productos.php">Productos</a>
+         </li>
+         <li>
+            <a href="contacto.php">Contacto</a>
+         </li>
+         <li>
+            <a href="facebook">Facebook</a>
+         </li>
+    </ul>
+</nav>
