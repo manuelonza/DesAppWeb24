539fca7de2eab3cc82f87946799e729e26ac6109
Miguel Esteban <miguelestebn@gmail.com>
2024-01-30 20:41:39 +0100

    jg

--- PHP/8_calendar/index2.php
+++ PHP/8_calendar/index2.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calendario Gim</title>
+    <title>Calendario Gim 2</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style2.css">
 </head>
 <body>
-<header>
+    <header>
         <h1>Calendario Gim</h1>
         <nav>
             <ul>
@@ -18,59 +18,73 @@
         </nav>
 </header>
 
-<?php
+<main>
 
+<?php
 
 // Cargar y leer el contenido del JSON
 $jsonData = file_get_contents('actividades.json');
 $data = json_decode($jsonData, true);
 
-
-
-
 ?>
 
 <div class="semana">
-    <?
+    <?php
+
+
+    $arrayJS='';
+
     
+    $i=0;
     foreach($data['calendario'] as $dia){
  
     echo '<div id="'.$dia['dia'].'">';
     echo '<h2>'.$dia['dia'].'</h2>';
     echo '<ul>';
 
+
         foreach($dia['actividades'] as $actividad){
 
-            echo '<li class="'.$actividad['categoria'].'" onclick="modal(this)">
+            echo '<li class="'.$actividad['categoria'].'" onclick="cargar('.$i.')">
             <h3>'.$actividad['actividad'].'</h3>
             <p class="hora"><i class="fa-solid fa-clock"></i>'.$actividad['hora'].'</p>
             <p class="duracion"><i class="fa-solid fa-hourglass-half"></i>'.$actividad['duracion'].'min</p>
             <p class="sala"><i class="fa-solid fa-location-dot"></i>'.$actividad['sala'].'</p>
-            <p class="monitor"><i class="fa-solid fa-user"></i>Monitor: '.$actividad['monitor'].'</p>
+            </li>';
 
+            // Tu bucle o lógica para agregar elementos al array
+            $arrayJS.='
+            ["'.$actividad['actividad'].'","'.$actividad['categoria'].'","'.$actividad['foto'].'","'.$actividad['hora'].'","'.$actividad['duracion'].'","'.$actividad['sala'].'","'.$actividad['monitor'].'","'.$actividad['descripcion'].'"],';
 
-    <div class="modal desactivado">
-    <div class="ventana '.$actividad['categoria'].'"> 
-        <div class="cerrar"><i class="fa-solid fa-xmark"></i></div>   
-        <img src="img/'.$actividad['foto'].'" alt="'.$actividad['actividad'].'">
-        <h3>'.$actividad['actividad'].'</h3>
-                <p class="hora"><i class="fa-solid fa-clock"></i>'.$actividad['hora'].'</p>
-                <p class="duracion"><i class="fa-solid fa-hourglass-half"></i>'.$actividad['duracion'].'min</p>
-                <p class="sala"><i class="fa-solid fa-location-dot"></i>'.$actividad['sala'].'</p>
-                <p class="monitor"><i class="fa-solid fa-user"></i>Monitor: '.$actividad['monitor'].'</p>
-                <p class="descripcion"><i class="fa-solid fa-person-running"></i></i>Descripcion: '.$actividad['descripcion'].'</p>
-        </div>
-    </div>
+        
 
-        </li>';
+           
+            $i++;
         }
- 
-    echo '</ul>';
-    echo '</div>';
-}
-    ?>
-</div>
+        
+        echo '</ul>';
+        echo '</div>';
+        
+    }
 
+        // Eliminar la coma al final del último elemento
+        $arrayJS = rtrim($arrayJS, ',');
+    
+    echo '<script>
+    let actividades =[
+    '.$arrayJS.'
+];
+    </script>';
+    
+    
+    ?>
+</div> <!-- semana -->
+
+<div id="dato">
+
+
+</div> <!-- dato -->
+</main>
     
     <script src="script.js"></script>
 </body>
--- PHP/8_calendar/script.js
+++ PHP/8_calendar/script.js
@@ -1,3 +1,19 @@
 function modal(evento) {
     evento.classList.toggle('activo');
 }
+
+function cargar(i){
+    let html="";
+    html+=`
+    <h1>${actividades[i][0]}</h1>
+    <img src="img/${actividades[i][2]}">
+    <p class="hora">${actividades[i][3]}</p>
+    <p class="duracion">${actividades[i][4]}</p>
+    <p class="descripcion">${actividades[i][7]}</p>
+    <p class="sala">${actividades[i][5]}</p>
+    <p class="monitor">${actividades[i][6]}</p>
+    `
+    document.querySelector('#dato').innerHTML=html;
+}
+
+cargar(0)
