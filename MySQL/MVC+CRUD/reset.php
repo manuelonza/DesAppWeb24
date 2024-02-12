d93f0495e13d855857d915502fa8db70dc2fd686
Miguel Esteban <miguelestebn@gmail.com>
2024-02-12 19:54:37 +0100

    a

--- MySQL/MVC+CRUD/reset.php
+++ MySQL/MVC+CRUD/reset.php
@@ -2,7 +2,9 @@
 <?php require '_functions.php'; ?>
 
 <h1><?echo $titulo;?></h1>
-<p>Vamos a resetear todo el contenido de la base de datos</p>
+<p>Vamos a resetear todo el contenido de la base de datos.</p>
+<p>Para que esto funcine, tienes que tener creada la base de datos "miBaseDeDatos" en tu servidor MySQL</p>
+<p>Verifica en functions.php que las claves de acceso a la BBDD son correctas, por defecto: localhost, root y root</p>
 
 
 <a href="borrarTabla.php" class="btn">Borrar tabla alumnos</a>
