81ae2b465f5689387ccdcef671d3f35c76067d87
Miguel Esteban <miguelestebn@gmail.com>
2024-02-14 15:46:11 +0100

    a

--- MySQL/MVC+CRUD/assets/inc/_header.php
+++ MySQL/MVC+CRUD/assets/inc/_header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $titulo;?></title>
-    <link rel="stylesheet" href="<? cargar('style') ?>">
+    <link rel="stylesheet" href="<? cargar('style'); echo'?='. time() ?>">
 </head>
 <body>
 
